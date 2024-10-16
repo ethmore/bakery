d8481973b290978e3dd1985d9c304d997bea9eae
ethmore <cetinermehmetali@gmail.com>
2024-10-16 14:38:20 +0300

    change prefix

--- prefix.ts
+++ prefix.ts
@@ -1,4 +1,4 @@
-// const prefix = "bakery";
-const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';;
+const prefix = "bakery";
+// const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';;
 
 export { prefix };
